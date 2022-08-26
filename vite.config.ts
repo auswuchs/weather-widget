ec7e03b3d05c31670a411f8fc15daf1ad937d44b
auswuchs <auswuchs@protonmail.com>
2022-08-26 16:03:41 +0300

    pre-deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-widget/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        inlineDynamicImports: true,
+        entryFileNames: 'weather-widget.js',
+      },
+    },
   }
 })
